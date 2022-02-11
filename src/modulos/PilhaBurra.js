4565978fe7a2e6c2a0b72d815c8f4800f27f9bdf
Articusss <Articusss@gmail.com>
2022-02-11 16:46:39 -0300

    Adicionada pilhaburra

---
+++ src/modulos/PilhaBurra.js
@@ -0,0 +1,37 @@
+//Criando a pilha burra
+function PilhaBurra(tamanho){
+    //Criando o array e "simulando" alocacao dinamica do C++. Preenche com undefined
+    const pilha = Array(tamanho).fill();
+    let topo = 0;
+
+    //Verifica cheia
+    function cheia(){
+        return topo == tamanho;
+    }
+
+    //Verifica vazia
+    function vazia(){
+        return topo == 0;
+    }
+
+    //Empilha
+    function push(item){
+        if(!cheia()){
+            pilha[topo++] = item;
+        } else {
+            console.error('Pilha cheia')
+        }
+    }
+
+    //Desempilha
+    function pop(){
+        topo--;
+        return pilha[topo];
+    }
+
+    function getPilha(){
+        return pilha;
+    }
+
+    return { cheia, vazia, push, pop }
+}
