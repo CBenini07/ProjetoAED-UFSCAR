a5cfc3c0baf8c02c2cea8fcc194b0bfc1dcd640e
mauricio almeida <galmeidamauricio12@gmail.com>
2022-03-07 22:50:00 -0300

    mensagem que eu quis

--- src/modulos/FilaBurra.js
+++ src/modulos/FilaBurra.js
@@ -1,36 +1,58 @@
-//FILA TEMPORARIA ENQUANTO MAURICIO NAO DA PUSH
-
+//Criando Fila burra
 function FilaBurra(tamanho){
-    const pilha = Array(tamanho).fill();
-    let topo = 0;
 
+    // Fila em forma de vetor - simulando alocacao dinamica. Preenchimento com undefined
+    const fila = Array(tamanho).fill();
+    let fim = 0; // fim (representa total de elementos e onde adicionar novo elemento)
+
+    // Verifica se esta vazia
     function vazia(){
-        return topo == 0;
+        return fim == 0;
     }
 
+    // Verifica se esta cheia
     function cheia(){
-        return topo == tamanho;
+        return fim == tamanho;
     }
 
-    function empilha(x){
-        if(!cheia()){
-            pilha[topo] = x;
-            topo++
-            return;
-        }
-        console.error("Fila cheia");
+    // Adiciona na fila
+    function push(elemento){
+        if (!cheia()){           
+            fila[fim] = elemento;
+            fim += 1;   
+        } 
+
+        else {
+            console.error("Fila cheia!");
+        }    
     }
 
-    function desempilha(){
-        if(!vazia()){
-            const valor = pilha[0];
-            for(let i=0; i < topo; i++) pilha[i] = pilha[i + 1];
-            topo--;
-            return valor;
+    // Retira da fila
+    function pop(){
+        if (!vazia()){
+            let elemento = fila[0];
+            fim -= 1;
+            
+            // deslocando elementos da direita pra esquerda
+            for(let i = 0; i < tamanho; i++)
+                fila[i] = fila[i + 1];
+
+            return elemento;
         }
-        console.error("Fila Vazia");
+
+        else{
+            console.error("Fila vazia!");
+        }
     }
-    return {desempilha, empilha, vazia, cheia}
+
+    // criada para testagem - remover dps
+    function imprime(){
+        for(let i = 0; i < tamanho; i++){
+            console.log(fila[i]);
+        }
+    }
+
+    return { vazia, cheia, push, pop, imprime };
 }
 
-export { FilaBurra }
+export{FilaBurra}
---
+++ src/modulos/FilaInteligente.js
@@ -0,0 +1,94 @@
+import {FilaBurra} from "./FilaBurra.js"
+
+function FilaInteligente(tamanho){
+    const fila = FilaBurra(tamanho);
+
+    // verifica se elemento esta presente na fila
+    function verificaElemento(elemento){
+        let valor, achou = false;
+
+        // retirando, comparando e repondo ao fim - rotacao completa, lista volta ao original
+        for(let i = 0; i < tamanho; i++){
+            
+            valor = fila.pop();
+
+            if (valor == elemento)
+                achou = true;
+
+            fila.push(valor); // reinserindo ao fim
+        }
+
+        return achou;
+    }
+
+    // verifica se um dado elemento esta na posicao dada
+    function verificaPosicao(elemento, posicao){
+        let valor, igual = false;
+
+        // retira, compara e adiciona ao fim
+        // ao fim do loop, rotacao completa eh feita e fila retorna posicoes
+        // originais
+        for(let i = 0; i < tamanho; i++){
+            valor = fila.pop();
+            if( i == posicao )
+                igual = (valor == elemento);
+            fila.push(valor);
+        }
+
+        return igual;
+    }   
+
+    // teste se fila atual e fila passada sao iguais
+    function filasIguais(fila2){
+        let valor1, valor2, iguais = true;
+        
+        // retirando de ambas filas, comparando e adicionando ao fim
+        for(let i = 0; i < tamanho; i++){
+            valor1 = fila.pop();
+            valor2 = fila2.pop();
+
+            if(valor1 != valor2)
+                iguais = false;
+            
+            fila.push(valor1);
+            fila2.push(valor2);
+        }       
+        
+        return iguais;
+    }
+
+    //Inserindo metodos da filaburra, assim como metodos da filainteligente
+    return Object.assign({}, fila, {filasIguais, verificaElemento, verificaPosicao});
+}
+
+export{FilaInteligente}
+
+/*
+codigo pra testagem no arquivo de implementacao: 
+fila - fila do usuario
+fila2- fila da palavra padrao
+
+let cor, elemento, i = 0;
+while(!fila.vazia())
+{
+    elemento = fila.pop();
+
+    if(fila2.verificaElemento(elemento)){
+        cor = 1; // 1 = amarelo
+
+        if(fila2.verificaPosicao(elemento, i)){;
+            cor = 2; // 2 - verde
+            console.log(elemento + " esta na posicao certa");
+        }
+        else    
+            console.log(elemento + " esta na palavra mas em posicao errada");
+    }
+
+    else{
+        console.log(elemento + " nao esta na palavra");
+    }
+    i++;
+}
+
+
+*/
